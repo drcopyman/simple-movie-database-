4c04f964f765cdc702e648474704f5eca0976f3a
drcopyman <wutthipong.luangsanam@gmail.com>
2020-09-27 18:52:49 +0700

    add logo , sceach , css adjust

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "./App-Style.scss";
+
+import HeaderLogo from './assets/logo.svg';
 import MovieCard from "./components/MovieCard/MovieCard";
 
 const CallPopularAPI =
@@ -9,8 +11,11 @@ const CallPopularAPI =
 
 function App() {
   const [MovieData, setMovieData] = useState([]);
-
-  useEffect(() => {
+  const [SceachFill, setSceachFill] = useState('');
+  const CallAPISceach = 
+`https://api.themoviedb.org/3/search/movie?api_key=229a6a0f891df5bf1176a4668af885c6&language=en-US&query=${SceachFill}&page=1&include_adult=false`
+  
+useEffect(() => {
     axios
       .get(CallPopularAPI)
       .then((res) => {
@@ -21,17 +26,51 @@ function App() {
       });
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if(SceachFill) {
+      axios
+      .get(CallAPISceach)
+      .then((res) => {
+        setMovieData(res.data.results);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+      setSceachFill('')
+    }
+    
+  };
+
+  const handleChange = (e) => {
+    setSceachFill(e.target.value);
+    
+  }
   return (
-    <div className="movie-container">
-      {MovieData.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          title={movie.title}
-          vote_average={movie.vote_average}
-          overview={movie.overview}
-          poster_path={movie.poster_path}
-        ></MovieCard>
-      ))}
+    <div>
+      <header>
+        <img src={HeaderLogo} alt="Logo"/>
+        <form onSubmit={handleOnSubmit}>
+          <input 
+          className="search" 
+          type="search" 
+          placeholder="Search..."  
+          value={SceachFill}
+          onChange={handleChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {MovieData.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            vote_average={movie.vote_average}
+            overview={movie.overview}
+            poster_path={movie.poster_path}
+          ></MovieCard>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./MovieCard-Style.scss";
 const img_path = "https://image.tmdb.org/t/p/w500";
+
 const MovieCard = ({ title, vote_average, overview, poster_path }) => {
+  
+  const tagVote = () => {
+    if (vote_average >= 8) {
+        return 'good'    
+    } else if (vote_average < 8 && vote_average > 5) {
+      return 'normal'
+    } else {
+      return 'bad'
+    }
+  };
   return (
     <div className="movie-card">
       <img src={`${img_path}/${poster_path}`} alt={`${title}`} />
       <div className="movie-info">
         <h3>{`${title}`}</h3>
-        <span className="good">{`${vote_average}`}</span>
+        <span className={`tag ${tagVote()}`}>{`${vote_average}`}</span>
       </div>
 
       <div className="movie-overview">
