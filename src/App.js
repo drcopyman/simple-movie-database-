def0ba665d217355a8ab5432785c8b1e4ace307c
drcopyman <wutthipong.luangsanam@gmail.com>
2020-09-27 17:09:51 +0700

    Add SASS , MovieCard , and passprops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import MovieCard from "./components/MovieCard";
+
+const CallPopularAPI =
+  "https://api.themoviedb.org/3/trending/all/week?api_key=229a6a0f891df5bf1176a4668af885c6";
 
 function App() {
+  const [MovieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(CallPopularAPI)
+      .then((res) => {
+        setMovieData(res.data.results);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
-    <div className="App">
-    
+    <div>
+      {MovieData.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          vote_average={movie.vote_average}
+          overview={movie.overview}
+          poster_path={movie.poster_path}
+        ></MovieCard>
+      ))}
     </div>
   );
 }
